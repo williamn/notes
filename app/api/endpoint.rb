c11c43694c0c6de9e646daf28295b62789d73bdb
William Notowidagdo <wnotowidagdo@gmail.com>
2014-06-17 19:21:35 +0700

    fixed #1: invalid version in Accept-Version header would return HTTP code 404 and error message instead of Rails routing error exception

--- app/api/endpoint.rb
+++ app/api/endpoint.rb
@@ -5,5 +5,9 @@ module Endpoint
   class API < Grape::API
     mount Notes::APIv2
     mount Notes::APIv1
+
+    route :any, '*path' do
+      error!({ error: "The requested item doesn't exist"}, 404)
+    end
   end
 end
